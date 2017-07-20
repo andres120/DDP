1dec72d3c9069e65b43f47f45eeaea682f9d8780
andres120 <andres-120@msn.com>
2017-07-21 01:36:02 +0800

    Add files via upload
    Added usage instructions within the UI.

--- ui.R
+++ ui.R
@@ -24,6 +24,17 @@ shinyUI(
     titlePanel("Latency Visualizer"),
     fluidRow(
       column(width = 11,
+             "\tUsage: The following application allows to quickly visualize disk I/O 
+      latency distribution across physical disk (LUNs) and also its trend, 
+             both can be filtered by LUN name using the LUN Filter text input and by time 
+             span by setting a time range using the Time frame sliders."
+      ),
+      column(width = 11,
+             ""
+      )
+    ),
+    fluidRow(
+      column(width = 11,
              textInput("filter","LUN Filter:",
                        placeholder = 
                          "Use regex i.e. LUN[5-8]|LUN13 will return LUNs 5 to 8 and 13", 
